5caba199ff4c3232e3ba820b30bbbe94070bc770
Rasheedat Jamiu <hartinuke16@gmail.com>
2023-03-08 18:18:26 +0100

    display public dns

--- aws_module/gapped_ec2/output.tf
+++ aws_module/gapped_ec2/output.tf
@@ -5,3 +5,7 @@ output "private_dns" {
 output "Public_ip" {
   value = zipmap(aws_instance.ec2_instance.*.tags.Name, aws_instance.ec2_instance.*.private_ip)
 }
+
+output "public_dns" {
+  value = zipmap(aws_instance.ec2_instance.*.tags.Name, aws_instance.ec2_instance.*.public_dns)
+}
