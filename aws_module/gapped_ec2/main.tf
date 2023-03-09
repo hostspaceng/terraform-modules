f519e9db3c0e84a1c61842b88521af61f705310a
Rasheedat Jamiu <hartinuke16@gmail.com>
2023-03-09 23:25:54 +0100

    added ebs volume

--- aws_module/gapped_ec2/main.tf
+++ aws_module/gapped_ec2/main.tf
@@ -1,3 +1,7 @@
+######################################################
+##                creating instance                 ##
+######################################################     
+
 resource "aws_instance" "ec2_instance" {
   ami             = var.ami_id
   subnet_id       = var.subnet_id
@@ -11,6 +15,32 @@ resource "aws_instance" "ec2_instance" {
     Name = "${var.instance_name}-${count.index}"
   }
 }
+
+######################################################
+##               creating EBS volume                ##
+######################################################  
+
+resource "aws_ebs_volume" "ebs_volume" {
+  count = var.no_of_volumes
+  availability_zone = "us-east-1a"
+  size              = 50
+  type              = "gp2"
+}
+
+######################################################
+##    Attachmening the volume to the instance       ##
+######################################################  
+
+resource "aws_volume_attachment" "ebs_att" {
+  count       = 2
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs_volume.*.id[count.index]
+  instance_id = element(aws_instance.ec2_instance.*.id, ((count.index + 1) % 2))
+}
+
+######################################################
+##        creating Airgapped security group         ##
+######################################################  
 
 resource "aws_security_group" "security_group" {
 
@@ -30,7 +60,7 @@ resource "aws_security_group" "security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.100.0/24"] #could be modified to block of ips youll like to allow acces
 
   }
 
@@ -39,3 +69,4 @@ resource "aws_security_group" "security_group" {
   }
 
 }
+
--- aws_module/gapped_ec2/variable.tf
+++ aws_module/gapped_ec2/variable.tf
@@ -19,6 +19,11 @@ variable "number_of_instances" {
   default     = 2
 }
 
+variable "no_of_volumes" {
+  description = "number of volumes to be created"
+  default     = 2
+}
+
 variable "ami_id" {
   description = "The AMI to use (default ami is a red hat ami)"
   type        = string
@@ -28,12 +33,26 @@ variable "ami_id" {
 
 variable "key_pair" {
   description = "name of your keypair"
-  default     = "mykeypair"
+  default     = "airgapkey"
 }
 
 
 variable "subnet_id" {
   description = "The VPC subnet the instance(s) will be created in"
   type        = string
-  default     = "subnet-057a97a7c3626bf64"
+  default     = "Subnet-03620001daceb2376"
+}
+
+variable "ec2_storage" {
+  type = map(object({
+    size = number
+    type = string
+  }))
+
+  default = {
+    "ec2-storage" = {
+      size = 50
+      type = "gp2"
+    }
+  }
 }
